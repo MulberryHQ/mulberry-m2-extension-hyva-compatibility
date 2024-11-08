92387cf478f453798a25c9e3bef20a9389d27434
Dmitrijs Sitovs <dmitrijs@magecroco.com>
2024-11-08 16:41:17 +0200

    Hyva compatibility module, initial release

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @category Mulberry Team
+ * @package Mulberry Warranty Hyva Compatibility
+ * @author Mulberry <support@getmulberry.com>
+ * @copyright Copyright (c) 2024 Mulberry Technology Inc., Ltd (http://www.getmulberry.com)
+ * @license http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0 (OSL-3.0)
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Mulberry_WarrantyHyvaCompatibility', __DIR__);
